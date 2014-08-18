9570eef8650187718d740e351bcfccde220345ce
zhouj <357008558@qq.com>
2014-08-18 14:33:55 +0800

    sdk1.3.9 add proximity in beacon
    sdk1.3.9 add proximity in beacon

--- AprilBeaconDemos/src/com/aprilbrother/aprilbeacondemo/BeaconAdapter.java
+++ AprilBeaconDemos/src/com/aprilbrother/aprilbeacondemo/BeaconAdapter.java
@@ -19,70 +19,92 @@ public class BeaconAdapter extends BaseAdapter {
 
 	public BeaconAdapter(Context context) {
 		this.inflater = LayoutInflater.from(context);
-	    this.beacons = new ArrayList<Beacon>();
+		this.beacons = new ArrayList<Beacon>();
 	}
-	
+
 	public void replaceWith(Collection<Beacon> newBeacons) {
-	    this.beacons.clear();
-	    this.beacons.addAll(newBeacons);
-	    notifyDataSetChanged();
-	  }
+		this.beacons.clear();
+		this.beacons.addAll(newBeacons);
+		notifyDataSetChanged();
+	}
 
 	@Override
-	  public int getCount() {
-	    return beacons.size();
-	  }
+	public int getCount() {
+		return beacons.size();
+	}
 
-	  @Override
-	  public Beacon getItem(int position) {
-	    return beacons.get(position);
-	  }
+	@Override
+	public Beacon getItem(int position) {
+		return beacons.get(position);
+	}
 
-	  @Override
-	  public long getItemId(int position) {
-	    return position;
-	  }
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-	  @Override
-	  public View getView(int position, View view, ViewGroup parent) {
-	    view = inflateIfRequired(view, position, parent);
-	    bind(getItem(position), view);
-	    return view;
-	  }
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		view = inflateIfRequired(view, position, parent);
+		bind(getItem(position), view);
+		return view;
+	}
 
-	  private void bind(Beacon beacon, View view) {
-	    ViewHolder holder = (ViewHolder) view.getTag();
-	    holder.macTextView.setText(String.format("MAC: %s (%.2fm)", beacon.getMacAddress(), beacon.getDistance()));
-	    holder.uuidTextView.setText("UUID: "+ beacon.getProximityUUID());
-	    holder.majorTextView.setText("Major: " + beacon.getMajor());
-	    holder.minorTextView.setText("Minor: " + beacon.getMinor());
-	    holder.measuredPowerTextView.setText("MPower: " + beacon.getMeasuredPower());
-	    holder.rssiTextView.setText("RSSI: " + beacon.getRssi());
-	  }
+	private void bind(Beacon beacon, View view) {
+		ViewHolder holder = (ViewHolder) view.getTag();
+		holder.macTextView.setText(String.format("MAC: %s (%.2fm)",
+				beacon.getMacAddress(), beacon.getDistance()));
+		holder.uuidTextView.setText("UUID: " + beacon.getProximityUUID());
+		holder.majorTextView.setText("Major: " + beacon.getMajor());
+		holder.minorTextView.setText("Minor: " + beacon.getMinor());
+		holder.measuredPowerTextView.setText("MPower: "
+				+ beacon.getMeasuredPower());
+		holder.rssiTextView.setText("RSSI: " + beacon.getRssi());
 
-	  private View inflateIfRequired(View view, int position, ViewGroup parent) {
-	    if (view == null) {
-	      view = inflater.inflate(R.layout.device_item, null);
-	      view.setTag(new ViewHolder(view));
-	    }
-	    return view;
-	  }
+		switch (beacon.getProximity()) {
+		case 0:
+			holder.proximityView.setText("proximity: " + "unknow");
+			break;
+		case 1:
+			holder.proximityView.setText("proximity: " + "immediate");
+			break;
+		case 2:
+			holder.proximityView.setText("proximity: " + "near");
+			break;
+		case 3:
+			holder.proximityView.setText("proximity: " + "far");
+			break;
+		default:
+			break;
+		}
 
-	  static class ViewHolder {
-	    final TextView macTextView;
-	    final TextView uuidTextView;
-	    final TextView majorTextView;
-	    final TextView minorTextView;
-	    final TextView measuredPowerTextView;
-	    final TextView rssiTextView;
+	}
 
-	    ViewHolder(View view) {
-	      macTextView = (TextView) view.findViewWithTag("mac");
-	      uuidTextView = (TextView) view.findViewWithTag("uuid");
-	      majorTextView = (TextView) view.findViewWithTag("major");
-	      minorTextView = (TextView) view.findViewWithTag("minor");
-	      measuredPowerTextView = (TextView) view.findViewWithTag("mpower");
-	      rssiTextView = (TextView) view.findViewWithTag("rssi");
-	    }
-	  }
+	private View inflateIfRequired(View view, int position, ViewGroup parent) {
+		if (view == null) {
+			view = inflater.inflate(R.layout.device_item, null);
+			view.setTag(new ViewHolder(view));
+		}
+		return view;
+	}
+
+	static class ViewHolder {
+		final TextView macTextView;
+		final TextView uuidTextView;
+		final TextView majorTextView;
+		final TextView minorTextView;
+		final TextView measuredPowerTextView;
+		final TextView rssiTextView;
+		final TextView proximityView;
+
+		ViewHolder(View view) {
+			macTextView = (TextView) view.findViewWithTag("mac");
+			uuidTextView = (TextView) view.findViewWithTag("uuid");
+			majorTextView = (TextView) view.findViewWithTag("major");
+			minorTextView = (TextView) view.findViewWithTag("minor");
+			measuredPowerTextView = (TextView) view.findViewWithTag("mpower");
+			rssiTextView = (TextView) view.findViewWithTag("rssi");
+			proximityView = (TextView) view.findViewWithTag("proximity");
+		}
+	}
 }
